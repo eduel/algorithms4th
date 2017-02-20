f76dead7525e0a4eae2f99c7a7863fabb7c1b9ca
eduel <eduel@eduel-PC>
2017-02-21 00:22:07 +0800

    adjust path

--- alg4th/common/src/main/java/com/juzi/Constant.java
+++ alg4th/common/src/main/java/com/juzi/Constant.java
@@ -6,10 +6,14 @@ public final class Constant {
 
 	private static final String data_dir = "algs4-data";
 
-	public static final String getFilePath() {
+	public static final String getParentPath() {
 		String workingDir = System.getProperty("user.dir");
 		File workingPath = new File(workingDir);
 		String parentPath = workingPath.getParent();
-		return new File(parentPath, data_dir).getPath();
+		return new File(parentPath, data_dir).getPath() + File.separatorChar;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getParentPath());
 	}
 }
