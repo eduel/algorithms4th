4a335ecd53a30adfc5618d4ab20ceb7fd5eaa619
liubo <liubo@ieyecloud.com>
2017-02-22 18:14:52 +0800

    to C1_2_16

--- alg4th/chapter1/src/main/java/com/juzi/chapter1_2/Chapter1_2.java
+++ alg4th/chapter1/src/main/java/com/juzi/chapter1_2/Chapter1_2.java
@@ -3,10 +3,13 @@ package com.juzi.chapter1_2;
 import java.awt.Color;
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import com.juzi.Constant;
 
+import edu.princeton.cs.algs4.Counter;
+import edu.princeton.cs.algs4.Date;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Interval1D;
 import edu.princeton.cs.algs4.Interval2D;
@@ -15,6 +18,7 @@ import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Transaction;
 
 public class Chapter1_2 {
 
@@ -28,12 +32,12 @@ public class Chapter1_2 {
 			}
 			double minDistance = 100 * 100;
 			Point2D a, b = null;
-			//!N
+			// !N
 			for (int i = 0; i < N - 1; i++) {
 				a = points[i];
 				for (int j = i + 1; j < N; j++) {
 					b = points[j];
-					if (i == j || a.equals(b))//the same point;
+					if (i == j || a.equals(b))// the same point;
 						continue;
 
 					double d = a.distanceTo(b);
@@ -66,7 +70,7 @@ public class Chapter1_2 {
 			int N = interval1ds.length;
 			Interval1D a, b = null;
 			int count = 0;
-			//N!
+			// N!
 			for (int i = 0; i < N - 1; i++) {
 				a = interval1ds[i];
 				for (int j = i + 1; j < N; j++) {
@@ -100,7 +104,7 @@ public class Chapter1_2 {
 		}
 	}
 
-	//TODO
+	// TODO
 	static class C1_2_3 {
 		static Interval2D[] createInterval2Ds(int N, double min, double max) {
 			Interval2D[] interval2ds = new Interval2D[N];
@@ -126,7 +130,8 @@ public class Chapter1_2 {
 			for (int i = 0; i < N; i++) {
 				interval2ds[i].draw();
 			}
-			int intersectsCounts = 0, containsCounts = 0;//???do not exposure x and y?
+			int intersectsCounts = 0, containsCounts = 0;// ???do not exposure x
+															// and y?
 			Interval2D a = null, b = null;
 			for (int i = 0; i < N - 1; i++) {
 				a = interval2ds[i];
@@ -187,7 +192,7 @@ public class Chapter1_2 {
 		}
 	}
 
-	//circular rotation;
+	// circular rotation;
 	static class C1_2_6 {
 		static boolean cr(String s, String t) {
 			return (s.length() == t.length()) && (s.concat(s).indexOf(t) > 0);
@@ -197,6 +202,168 @@ public class Chapter1_2 {
 			String s = "ABCDE";
 			String t = "DEABC";
 			StdOut.println(cr(s, t));
+		}
+	}
+
+	static class C1_2_7 {
+		// reverse;
+		static String mystery(String s) {
+			int N = s.length();
+			if (N <= 1)
+				return s;
+			String a = s.substring(0, N / 2);
+			String b = s.substring(N / 2, N);// [a,b);
+			return mystery(b) + mystery(a);
+		}
+
+		public static void main(String[] args) {
+			String s = "ABCDEF";
+			StdOut.println(mystery(s));
+			String s2 = "abcdefghijklmnop";
+			StdOut.println(mystery(s2));
+		}
+	}
+
+	static class C1_2_8 {
+		//
+		static void referenceSwap(int[] a, int[] b) {
+			int[] t = a;
+			a = b;
+			b = t;
+		}
+	}
+
+	static class C1_2_9 {
+		static int indexOf(int[] a, int key, Counter counter) {
+			int lo = 0;
+			int hi = a.length - 1;
+			while (lo <= hi) {
+				// Key is in a[lo..hi] or not present.
+				int mid = lo + (hi - lo) / 2;
+				counter.increment();
+				if (key < a[mid])
+					hi = mid - 1;
+				else if (key > a[mid])
+					lo = mid + 1;
+				else
+					return mid;
+			}
+			return -1;
+		}
+
+		public static void main(String[] args) {
+			Counter counter = new Counter("key counter");
+			In in = new In(Constant.getParentPath() + "largeW.txt");
+			int[] ints = in.readAllInts();
+			Arrays.sort(ints);
+			StdOut.println("enter key:");
+			int key = StdIn.readInt();
+			int index = indexOf(ints, key, counter);
+			StdOut.println(String.format("find key=%d in index=%d use %d times", key, index, counter.tally()));
+		}
+	}
+
+	// ???
+	static class C1_2_10 {
+		static class VisualCounter {
+			int N;
+			int max;
+			int count;
+			int operationCount;
+
+			public VisualCounter(int N, int max) {
+				this.N = N;
+				this.max = max;
+			}
+
+			void increment() {
+				check();
+				count++;
+				operationCount++;
+			}
+
+			void decrement() {
+				count--;
+			}
+
+			void reset() {
+				count = 0;
+				operationCount = 0;
+			}
+
+			void check() {
+				if (count == max || operationCount == N)
+					throw new UnsupportedOperationException();
+			}
+		}
+	}
+
+	// TODO
+	static class C1_2_11 {
+
+	}
+
+	// TODO
+	static class C1_2_12 {
+
+	}
+
+	static class C1_2_13 {
+		public static void main(String[] args) {
+			Transaction transaction;
+		}
+	}
+
+	static class C1_2_14 {
+		public static void main(String[] args) {
+			Transaction t = new Transaction("eduel", new Date("02/22/2017"), 9527.77);
+			Transaction t2 = new Transaction("eduel", new Date("02/22/2017"), 9527.77);
+			StdOut.println(t.equals(t2));
+		}
+	}
+
+	static class C1_2_15 {
+		// only supporte use \\s as split char;
+		static int[] readInts(String name) {
+			In in = new In(name);
+			String input = in.readAll();
+			String[] splited = input.split("\\s+");//
+			int[] ints = new int[splited.length];
+			for (int i = 0; i < ints.length; i++) {
+				ints[i] = Integer.valueOf(splited[i]);
+			}
+			return ints;
+		}
+	}
+
+	static class C1_2_16 {
+		static class Rational {
+			int numerator;
+			int denomerator;
+
+			public Rational(int numerator, int denomerator) {
+				super();
+				this.numerator = numerator;
+				this.denomerator = denomerator;
+			}
+
+			//TODO
+			Rational plus(Rational b) {
+				return null;
+			}
+
+			static long gcd(long p, long q) {
+				if (q == 0)
+					return p;
+				if (p % q == 0)
+					return q;
+				while (q > 0) {
+					long r = p % q;
+					p = q;
+					q = r;
+				}
+				return p;
+			}
 		}
 	}
 
