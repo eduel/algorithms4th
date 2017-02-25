b82883193b86dab2e6180a98bb9bdcbd6dc94053
eduel <eduel@eduel-PC>
2017-02-26 04:39:30 +0800

    to C1_3_14

--- alg4th/chapter1/src/main/java/com/juzi/chapter1_3/Chapter1_3.java
+++ alg4th/chapter1/src/main/java/com/juzi/chapter1_3/Chapter1_3.java
@@ -1,7 +1,12 @@
 package com.juzi.chapter1_3;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import com.juzi.chapter1_3.Chapter1_3.C1_3_10.InfixToPostfix;
+import com.juzi.chapter1_3.Chapter1_3.C1_3_4.Parenthess.Pair;
 
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
@@ -47,50 +52,64 @@ public class Chapter1_3 {
 	static class C1_3_4 {
 		static class Parenthess {
 			static enum Pair {
-				P1('{', '}'), P2('(', ')'), P3('[', ']');
-				final char left;
-				final char right;
+				P1("{", "}"), P2("(", ")"), P3("[", "]");
+				String left;
+				String right;
+				static String lefts = "([{";
+				static String rights = ")]}";
 
-				private Pair(char left, char right) {
+				private Pair(String left, String right) {
 					this.left = left;
 					this.right = right;
 				}
 
-				static char getRight(char left) {
-					char right;
-					if (left == P1.left)
+				static String getRight(String left) {
+					if (lefts.indexOf(left) < 0)
+						throw new IllegalArgumentException("not left");
+					String right;
+					if (P1.left.equals(left))
 						right = P1.right;
-					else if (left == P2.left)
+					else if (P2.left.equals(left))
 						right = P2.right;
 					else
 						right = P3.right;
 					return right;
 				}
 
-				static char getLeft(char right) {
-					char left;
-					if (right == P1.right)
+				static String getLeft(String right) {
+					if (rights.indexOf(right) < 0)
+						throw new IllegalArgumentException("not right");
+					String left;
+					if (P1.right.equals(right))
 						left = P1.left;
-					else if (right == P2.right)
+					else if (P2.right.equals(right))
 						left = P2.left;
 					else
 						left = P3.left;
 					return left;
 				}
+
+				static boolean isLeft(String input) {
+					return lefts.indexOf(input) != -1;
+				}
+
+				static boolean isRight(String input) {
+					return rights.indexOf(input) != -1;
+				}
 			}
 
-			static boolean isPair(String s) {
-				Stack<Character> stack = new Stack<>();
-				for (char c : s.toCharArray()) {
-					boolean isLeft = c == Pair.P1.left || c == Pair.P2.left || c == Pair.P3.left;
+			static boolean isPair(String[] input) {
+				Stack<String> stack = new Stack<>();
+				for (String s : input) {
+					boolean isLeft = Pair.isLeft(s);
 					if (isLeft) {
-						stack.push(c);
+						stack.push(s);
 					} else {
 						if (stack.isEmpty())
 							return false;
-						char leftC = stack.pop();
-						char leftC2 = Pair.getLeft(c);
-						if (leftC != leftC2)
+						String leftC = stack.pop();
+						String leftC2 = Pair.getLeft(s);
+						if (!leftC.equals(leftC2))
 							return false;
 					}
 				}
@@ -100,8 +119,8 @@ public class Chapter1_3 {
 			public static void main(String[] args) {
 				String s1 = "[()]{}{[()]()}";
 				String s2 = "[(])";
-				StdOut.println(isPair(s1));
-				StdOut.println(isPair(s2));
+				StdOut.println(isPair(s1.split("")));
+				StdOut.println(isPair(s2.split("")));
 			}
 		}
 	}
@@ -198,7 +217,7 @@ public class Chapter1_3 {
 			}
 			String result = valStack.pop();
 			String reverseResult = "";
-			for(int i=result.length()-1;i>=0;i--){
+			for (int i = result.length() - 1; i >= 0; i--) {
 				reverseResult += result.charAt(i);
 			}
 			StdOut.println(reverseResult);
@@ -210,5 +229,265 @@ public class Chapter1_3 {
 			print(splited);
 
 		}
+	}
+
+	//TODO
+	static class C1_3_10 {
+		//+,-,*,/,(,);
+		static class InfixToPostfix {
+			static List<String> ops = Arrays.asList("+", "-", "*", "/");
+			static String lowOP = "+-";
+			static String highOP = "*/";
+			static String plus = "+";
+			static String minus = "-";
+			static String mult = "*";
+			static String div = "/";
+
+			static int priority(String op) {
+				if (!ops.contains(op))
+					throw new IllegalArgumentException("not op");
+				return highOP.indexOf(op) == -1 ? 1 : 2;
+			}
+
+			static boolean isHigh(String op1, String op2) {
+				return priority(op1) >= priority(op2);
+			}
+
+			//input must be ok;
+			static String toPostfix(String[] input) {
+				Queue<String> expression = new Queue<>();
+				Stack<String> stack = new Stack<>();
+				for (String s : input) {
+					boolean isLeft = Pair.isLeft(s);
+					boolean isRight = Pair.isRight(s);
+					if (isLeft) {
+						stack.push(s);
+					} else if (isRight) {
+						if (stack.isEmpty())
+							throw new ArithmeticException();
+						while (!stack.isEmpty()) {
+							String temp = stack.pop();
+							if (temp.equals(Pair.getLeft(s)))
+								break;
+							expression.enqueue(temp);
+						}
+					} else if (ops.contains(s)) {
+						while (true) {
+							if (stack.isEmpty()) {
+								stack.push(s);
+								break;
+							}
+							String topOP = stack.peek();
+							boolean isOp = ops.contains(topOP);
+							if (isOp) {
+								boolean isHigh = isHigh(topOP, s);
+								if (isHigh) {
+									expression.enqueue(stack.pop());
+									continue;
+								}
+							} else {
+								boolean isNotLeft = !Pair.isLeft(topOP);
+								if (isNotLeft) {
+									expression.enqueue(stack.pop());
+									continue;
+								}
+							}
+							stack.push(s);
+							break;
+						}
+
+					} else {
+						expression.enqueue(s);
+					}
+				}
+
+				StringBuilder result = new StringBuilder();
+				for (String s : expression) {
+					result.append(s);
+				}
+				for (String s : stack) {
+					result.append(s);
+				}
+				return result.toString();
+			}
+
+			public static void main(String[] args) {
+				String s = "2*3/(2-1)+3*(4-1)";
+				StdOut.println(toPostfix(s.split("")));
+			}
+		}
+
+	}
+
+	static class C1_3_11 {
+		static int evaluatePostfix(String[] input) {
+			String postfix = C1_3_10.InfixToPostfix.toPostfix(input);
+			Stack<Integer> stack = new Stack<>();
+			for (String s : postfix.split("")) {
+				if (InfixToPostfix.ops.contains(s)) {
+					int c1 = stack.pop();
+					int c2 = stack.pop();
+					switch (s) {
+					case "+":
+						stack.push(c2 + c1);
+						break;
+					case "-":
+						stack.push(c2 - c1);
+						break;
+					case "*":
+						stack.push(c2 * c1);
+						break;
+					case "/":
+						stack.push(c2 / c1);
+						break;
+					default:
+						break;
+					}
+				} else {
+					stack.push(Integer.valueOf(s));
+				}
+			}
+			return stack.pop();
+		}
+
+		public static void main(String[] args) {
+			String s = "2*3/(2-1)+3*(4-1)";
+			String[] splited = s.split("");
+			StdOut.println(evaluatePostfix(splited));
+		}
+	}
+
+	static class C1_3_12 {
+		static class CopyableStack {
+
+			static Stack<String> copy(Stack<String> input) {
+				Stack<String> tempStack = new Stack<>();
+				Iterator<String> stackIterator = input.iterator();
+				while (stackIterator.hasNext()) {
+					String element = stackIterator.next();
+					tempStack.push(element);
+				}
+				Stack<String> copy = new Stack<>();
+				Iterator<String> tempStackIterator = tempStack.iterator();
+				while (tempStackIterator.hasNext()) {
+					copy.push(tempStackIterator.next());
+				}
+				return copy;
+			}
+
+			public static void main(String[] args) {
+				Stack<String> stack = new Stack<>();
+				stack.push("a");
+				stack.push("b");
+				stack.push("c");
+				Stack<String> copy = copy(stack);
+				for (String s : copy) {
+					StdOut.print(s);
+				}
+			}
+		}
+	}
+
+	static class C1_3_13 {
+		//b,c,d
+	}
+
+	static class C1_3_14 {
+		static class ResizingArrayQueueOfStrings implements Iterable<String> {
+			String[] vals;
+			int head;
+			int tail;
+			int n;
+
+			public ResizingArrayQueueOfStrings(int capacity) {
+				assert capacity > 0;
+				vals = new String[capacity];
+			}
+
+			boolean isEmpty() {
+				return n == 0;
+			}
+
+			int size() {
+				return n;
+			}
+
+			void enqueue(String e) {
+				if (n == vals.length)
+					vals = resize(vals.length * 2);
+				vals[tail++] = e;
+				if (tail == vals.length)
+					tail = 0;
+				n++;
+			}
+
+			String dequeue() {
+				if (isEmpty())
+					throw new NoSuchElementException();
+				String e = vals[head];
+				vals[head++] = null;
+				n--;
+				if (head == vals.length)
+					head = 0;
+				if (n > 0 && n == vals.length / 4)
+					resize(vals.length/2);
+				return e;
+			}
+
+			public String[] resize(int capacity) {
+				assert capacity >= n;
+				String[] newVals = new String[capacity];
+				for (int i = 0; i < n; i++) {
+					newVals[i] = vals[(head + i) % vals.length];//importent;
+				}
+				head = 0;
+				tail = n;
+				vals = newVals;
+				return vals;
+			}
+
+			@Override
+			public Iterator<String> iterator() {
+				return new Iterator<String>() {
+					int i = 0;
+
+					@Override
+					public boolean hasNext() {
+						return i < n;
+
+					}
+
+					@Override
+					public String next() {
+						if (!hasNext())
+							throw new NoSuchElementException();
+						return vals[(head + i++) % vals.length];
+					}
+				};
+
+			}
+
+		}
+
+		public static void main(String[] args) {
+			ResizingArrayQueueOfStrings queue = new ResizingArrayQueueOfStrings(2);
+			queue.enqueue("1");
+			queue.enqueue("2");
+			queue.enqueue("3");
+			for (String e : queue) {
+				StdOut.print(e);
+			}
+			StdOut.println();
+			queue.dequeue();
+			queue.dequeue();
+			queue.enqueue("4");
+			queue.enqueue("5");
+			//			queue.enqueue("6");
+			for (String e : queue) {
+				StdOut.print(e);
+			}
+
+		}
+
 	}
 }
